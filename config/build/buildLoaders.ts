3828a7428f0fb0d6d9ff17fc74a97e93316d2b1b
NikitaPokryshko <pokryshko.n@gmail.com>
2024-01-08 01:15:43 +0100

    Add SCSS/SASS loader
    Add React and test component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,16 +1,30 @@
 
-
 import webpack from 'webpack'
 
-
 export function buildLoaders(): webpack.RuleSetRule[] {
-  const typeScriptLoader =  {
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translate CSS into CommonJS,
+      "css-loader",
+      // Compiles Sass to CSS,
+      "sass-loader"
+    ]
+  }
+
+  // - With no typescript config babel is required to handle JSX
+  // - ts-loader handles JSX by default
+  const typescriptLoader =  {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   }
 
   return [
-    typeScriptLoader
+    typescriptLoader,
+    cssLoader
   ]
 }
