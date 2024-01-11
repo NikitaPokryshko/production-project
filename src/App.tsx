d502298c49d123cbe30ba5f8ce47d0d9c3e45aa7
NikitaPokryshko <pokryshko.n@gmail.com>
2024-01-11 02:05:38 +0100

    Add classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { Suspense, useContext, useState } from 'react'
+import { Suspense, } from 'react'
 import { Route, Routes, Link } from 'react-router-dom'
 
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy'
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy'
 
 import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classnames'
 
 import './styles/index.scss'
-
-export enum Theme {
-  LIGHT = 'light',
-  DARK = 'dark'
-}
 
 const App = () => {
   const { theme, toggleTheme} = useTheme()
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
